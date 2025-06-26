1a42a0f7cf7ce2096d71f1249e1cb7d871016273
StigMikalMJ-KP <lakkimgits@gmail.com>
2025-06-26 12:41:16 +0200

    Gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,43 +1,54 @@
-import React from 'react'
-import '../styling/gallery.css'
-import { useState } from 'react'
+import React, { useState } from 'react';
+import '../styling/gallery.css';
+
+const galleryImages = {
+  General: [
+    { src: '/images/general1.png', alt: 'General Image 1' },
+    { src: '/images/general2.png', alt: 'General Image 2' },
+  ],
+  GFX: [
+    { src: '/images/gfx1.png', alt: 'GFX Image 1' },
+    { src: '/images/gfx2.png', alt: 'GFX Image 2' },
+  ],
+  PRs: [
+    { src: '/images/prs1.png', alt: 'PRs Image 1' },
+    { src: '/images/prs2.png', alt: 'PRs Image 2' },
+  ],
+};
 
 const Gallery = () => {
-    const [choiceClicked, setChoiceClicked] = useState(null);
+  const [choiceClicked, setChoiceClicked] = useState('General');
 
-    const handleChoiceCLick = (choice) => {
-        setChoiceClicked(choice);
-        console.log(`Choice clicked: ${choice}`);
-    }
+  const handleChoiceClick = (choice) => {
+    setChoiceClicked(choice);
+    console.log(`Choice clicked: ${choice}`);
+  };
 
-
-    return (
-        <div id='gallery-page'>
-            <div id='gallery-choices'>
-                <div id='choices-container'>
-                    <button 
-                        className={`gallerybtn ${choiceClicked === 'General' ? 'active' : ''}`}
-                        onClick={() => handleChoiceCLick('General')}
-                    >General</button>
-
-                    <button 
-                        className={`gallerybtn ${choiceClicked === 'GFX' ? 'active' : ''}`}
-                        onClick={() => handleChoiceCLick('GFX')}
-                    >GFX</button>
-
-                    <button 
-                        className={`gallerybtn ${choiceClicked === 'PRs' ? 'active' : ''}`}
-                        onClick={() => handleChoiceCLick('PRs')}
-                    >PRs</button>
-
-                </div>
-            </div>
-
-            <div id='gallery-content'>
-
-            </div>
+  return (
+    <div id='gallery-page'>
+      <div id='gallery-choices'>
+        <div id='choices-container'>
+          {['General', 'GFX', 'PRs'].map((choice) => (
+            <button
+              key={choice}
+              className={`gallerybtn ${choiceClicked === choice ? 'active' : ''}`}
+              onClick={() => handleChoiceClick(choice)}
+            >
+              {choice}
+            </button>
+          ))}
         </div>
-    )
-}
+      </div>
 
-export default Gallery
+      <div id='gallery-content'>
+        <div className='gallery-images'>
+          {(galleryImages[choiceClicked] || []).map((img, idx) => (
+            <img key={idx} src={img.src} alt={img.alt} className='gallery-image' />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
